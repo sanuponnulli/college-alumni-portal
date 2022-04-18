27d2e8322a788dd426787be0db08f2965575b538
sanu <sanuponnulli@gmail.com>
2022-04-18 17:41:46 +0530

    mmm

--- base/views.py
+++ base/views.py
@@ -74,7 +74,7 @@ def home(request):
     room_messages=Message.objects.filter(Q(room__topic__name__icontains=q))
     context={'rooms':rooms,'topics':topics,'room_count':room_count,'room_messages':room_messages}
     return render(request,'base/home.html',context)
-
+@login_required(login_url='login')
 def room(request,pk):
     room=Room.objects.get(id=pk)
     room_messages=room.message_set.all().order_by('-created')
